a8a1e170f6abc2f51be7e70e0cb3b3825c2589f5
Mirko <mirkocolombo1994>
2024-08-19 21:34:40 +0200

    fix tests

--- src/main/java/com/swim/GreetingResource.java
+++ src/main/java/com/swim/GreetingResource.java
@@ -3,12 +3,11 @@ package com.swim;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/rest")
+@Path("/hello")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    @Path("hello")
     public String hello() {
         return "Hello from Quarkus REST";
     }
